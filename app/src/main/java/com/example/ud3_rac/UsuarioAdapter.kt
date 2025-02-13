aaf254ee9174a17ef08d298e8d97b803c57c92de
RubenArCol <arcoscolcheroruben@gmail.com>
2025-02-13 18:00:59 +0100

    feat/lista

---
+++ app/src/main/java/com/example/ud3_rac/UsuarioAdapter.kt
@@ -0,0 +1,51 @@
+package com.example.ud3_rac
+
+import androidx.recyclerview.widget.RecyclerView
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.TextView
+
+import com.example.ud3_rac.placeholder.PlaceholderContent.PlaceholderItem
+import com.example.ud3_rac.databinding.LineausuarioBinding
+
+/**
+ * [RecyclerView.Adapter] that can display a [PlaceholderItem].
+ * TODO: Replace the implementation with code for your data type.
+ */
+class UsuarioAdapter(
+    private val values: List<PlaceholderItem>
+) : RecyclerView.Adapter<UsuarioAdapter.ViewHolder>() {
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
+
+        return ViewHolder(
+            LineausuarioBinding.inflate(
+                LayoutInflater.from(parent.context),
+                parent,
+                false
+            )
+        )
+
+    }
+
+    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
+        val item = values[position]
+        holder.idView.text = item.id
+        holder.contentView.text = item.content
+        holder.fecha.text = item.details
+    }
+
+    override fun getItemCount(): Int = values.size
+
+    inner class ViewHolder(binding: LineausuarioBinding) : RecyclerView.ViewHolder(binding.root) {
+        val idView: TextView = binding.idUsuario
+        val contentView: TextView = binding.nombreUsuario
+        val fecha: TextView = binding.horaUsuario
+
+        override fun toString(): String {
+            return super.toString() + " '" + contentView.text + "'"
+        }
+    }
+
+}
--- app/src/main/java/com/example/ud3_rac/listFragment.kt
+++ app/src/main/java/com/example/ud3_rac/listFragment.kt
@@ -2,30 +2,26 @@ package com.example.ud3_rac
 
 import android.os.Bundle
 import androidx.fragment.app.Fragment
+import androidx.recyclerview.widget.GridLayoutManager
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-
-// TODO: Rename parameter arguments, choose names that match
-// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-private const val ARG_PARAM1 = "param1"
-private const val ARG_PARAM2 = "param2"
+import com.example.ud3_rac.placeholder.PlaceholderContent
 
 /**
- * A simple [Fragment] subclass.
- * Use the [listFragment.newInstance] factory method to
- * create an instance of this fragment.
+ * A fragment representing a list of Items.
  */
 class listFragment : Fragment() {
-    // TODO: Rename and change types of parameters
-    private var param1: String? = null
-    private var param2: String? = null
+
+    private var columnCount = 1
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
         arguments?.let {
-            param1 = it.getString(ARG_PARAM1)
-            param2 = it.getString(ARG_PARAM2)
+            columnCount = it.getInt(ARG_COLUMN_COUNT)
         }
     }
 
@@ -33,26 +29,32 @@ class listFragment : Fragment() {
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_list, container, false)
+        val view = inflater.inflate(R.layout.fragment_list, container, false)
+
+        // Set the adapter
+        if (view is RecyclerView) {
+            with(view) {
+                layoutManager = when {
+                    columnCount <= 1 -> LinearLayoutManager(context)
+                    else -> GridLayoutManager(context, columnCount)
+                }
+                adapter = UsuarioAdapter(PlaceholderContent.ITEMS)
+            }
+        }
+        return view
     }
 
     companion object {
-        /**
-         * Use this factory method to create a new instance of
-         * this fragment using the provided parameters.
-         *
-         * @param param1 Parameter 1.
-         * @param param2 Parameter 2.
-         * @return A new instance of fragment listFragment.
-         */
-        // TODO: Rename and change types and number of parameters
+
+        // TODO: Customize parameter argument names
+        const val ARG_COLUMN_COUNT = "column-count"
+
+        // TODO: Customize parameter initialization
         @JvmStatic
-        fun newInstance(param1: String, param2: String) =
+        fun newInstance(columnCount: Int) =
             listFragment().apply {
                 arguments = Bundle().apply {
-                    putString(ARG_PARAM1, param1)
-                    putString(ARG_PARAM2, param2)
+                    putInt(ARG_COLUMN_COUNT, columnCount)
                 }
             }
     }
